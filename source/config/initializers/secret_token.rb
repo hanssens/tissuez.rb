1454e50fb9242a639cef4bb7bf2235d9aefe4ba6
hanssens <github@hanssens.com>
2014-02-18 21:55:34 +0100

    Initial setup of the Tissuez project

---
+++ source/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tissuez::Application.config.secret_key_base = '545afaba0bae0a6cc5ff461890bd6d46ff409cfc16acd1cd4ed9a88b024969cccbe1f5fecb308d1129155033b784f93ad60bdaaa168ebca0e4941afa86a0ef63'
