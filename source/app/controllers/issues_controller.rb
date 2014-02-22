b00dcae6176eca7598797a26dd87a8e62232f9a7
JHanssens <github@hanssens.com>
2014-02-22 15:34:43 +0100

    Basic integration of Octokit

--- source/app/controllers/issues_controller.rb
+++ source/app/controllers/issues_controller.rb
@@ -2,6 +2,12 @@ class IssuesController < ApplicationController
 
   # default homepage
   def index
+
+	client = Octokit::Client.new :access_token => "..."
+
+	# return to view
+	@user = Octokit.user 'hanssens'
+
     render 'issues/home'
   end
 
